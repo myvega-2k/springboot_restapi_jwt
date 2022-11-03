b5dbf67aaf2759862eed9c57155302bf0d68c916
myvega-2k <vega2k_@daum.net>
2022-11-03 14:20:15 +0900

    dbrunner add

--- src/main/java/com/basic/myrestapi/MyRestApiApplication.java
+++ src/main/java/com/basic/myrestapi/MyRestApiApplication.java
@@ -1,5 +1,6 @@
 package com.basic.myrestapi;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +10,6 @@ public class MyRestApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MyRestApiApplication.class, args);
 	}
+
 
 }
---
+++ src/main/java/com/basic/myrestapi/common/DBRunner.java
@@ -0,0 +1,32 @@
+package com.basic.myrestapi.common;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+
+@Component
+@Order(1)
+@Slf4j
+public class DBRunner implements ApplicationRunner {
+	@Autowired
+	DataSource dataSource;
+	
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		log.debug("DataSource 구현체 = {}  ", dataSource.getClass().getName());
+		
+		Connection connection = dataSource.getConnection();
+		DatabaseMetaData metaData = connection.getMetaData();
+		log.debug("DB Vendor Name = {}", metaData.getDatabaseProductName());
+		log.debug("DB URL = {} ", metaData.getURL());
+		log.debug("DB Username = {} ", metaData.getUserName());
+	}
+}
+
