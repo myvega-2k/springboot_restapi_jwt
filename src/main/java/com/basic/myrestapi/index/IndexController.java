5f55f4e35bcd617edc8809916d0e28a81ce82bc7
myvega-2k <vega2k_@daum.net>
2022-11-03 17:24:56 +0900

    indexcontroller add

---
+++ src/main/java/com/basic/myrestapi/index/IndexController.java
@@ -0,0 +1,18 @@
+package com.basic.myrestapi.index;
+
+import com.basic.myrestapi.lectures.LectureController;
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+
+@RestController
+public class IndexController {
+    @GetMapping("/api")
+    public RepresentationModel index() {
+        var index = new RepresentationModel();
+        index.add(linkTo(LectureController.class).withRel("lectures"));
+        return index;
+    }
+}
