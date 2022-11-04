6a690dc8331f53412c9cb5613ab9e53affa03c98
myvega-2k <vega2k_@daum.net>
2022-11-04 09:19:58 +0900

    test update

--- src/test/java/com/basic/myrestapi/LambdaTest.java
+++ src/test/java/com/basic/myrestapi/LambdaTest.java
@@ -3,9 +3,23 @@ package com.basic.myrestapi;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.function.Consumer;
+
 public class LambdaTest {
     @Test
     public void consumer() {
+        //Immutable List
+        List<String> list = List.of("aa", "bb", "ccc");
+        //list.add("dd");
+        //1. Anonymous Inner Class
+        list.forEach(new Consumer<String>() {
+            @Override
+            public void accept(String s) {
+                System.out.println("s = " + s);
+            }
+        });
+        //2. Lambda Expression
 
     }
 
