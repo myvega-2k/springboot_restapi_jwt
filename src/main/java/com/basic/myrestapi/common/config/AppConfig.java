c05e8a8927fb86504c59e4761f19d79fb0ab9f61
myvega-2k <vega2k_@daum.net>
2022-11-04 15:43:26 +0900

    accountAdapter add

--- src/main/java/com/basic/myrestapi/common/config/AppConfig.java
+++ src/main/java/com/basic/myrestapi/common/config/AppConfig.java
@@ -28,7 +28,6 @@ public class AppConfig {
         return new ApplicationRunner() {
             @Autowired
             AccountService accountService;
-
             @Autowired
             AppProperties appProperties;
 
