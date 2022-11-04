a54d21ebfb7b3576509fbfbec6805abca853ee3c
myvega-2k <vega2k_@daum.net>
2022-11-04 15:16:09 +0900

    appconfig add

---
+++ src/main/java/com/basic/myrestapi/common/config/AppConfig.java
@@ -0,0 +1,41 @@
+package com.basic.myrestapi.common.config;
+
+import com.basic.myrestapi.accounts.Account;
+import com.basic.myrestapi.accounts.AccountRole;
+import com.basic.myrestapi.accounts.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Set;
+
+@Configuration
+public class AppConfig {
+    
+    //패스워드 인코딩 해주는 PasswordEncode를 Bean으로 등록
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public ApplicationRunner applicationRunner() {
+        return new ApplicationRunner() {
+            @Autowired
+            AccountService accountService;
+            @Override
+            public void run(ApplicationArguments args) throws Exception {
+                Account account = Account.builder()
+                        .email("user@email.com")
+                        .password("user")
+                        .roles(Set.of(AccountRole.ADMIN,AccountRole.USER))
+                        .build();
+                accountService.saveAccount(account);
+            }
+        };
+    }
+}
