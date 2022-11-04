aeed9e7782e0966237c0117a89f5e18b804862f2
myvega-2k <vega2k_@daum.net>
2022-11-04 14:59:07 +0900

    accountService add

---
+++ src/main/java/com/basic/myrestapi/accounts/AccountService.java
@@ -0,0 +1,20 @@
+package com.basic.myrestapi.accounts;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountService {
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    public Account saveAccount(Account account) {
+        account.setPassword(this.passwordEncoder.encode(account.getPassword()));
+        return this.accountRepository.save(account);
+    }
+
+}
