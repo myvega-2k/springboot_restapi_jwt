d9066a4048bb0dd541482116f135295e160ff73f
myvega-2k <vega2k_@daum.net>
2022-11-04 15:18:36 +0900

    service update

--- src/main/java/com/basic/myrestapi/accounts/AccountService.java
+++ src/main/java/com/basic/myrestapi/accounts/AccountService.java
@@ -1,11 +1,21 @@
 package com.basic.myrestapi.accounts;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @Service
-public class AccountService {
+public class AccountService implements UserDetailsService {
     @Autowired
     private AccountRepository accountRepository;
 
@@ -15,6 +25,17 @@ public class AccountService {
     public Account saveAccount(Account account) {
         account.setPassword(this.passwordEncoder.encode(account.getPassword()));
         return this.accountRepository.save(account);
+    }
+
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Account account = accountRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username));
+        return new User(account.getEmail(), account.getPassword(),
+                authorities(account.getRoles()));
+    }
+    private Collection<? extends GrantedAuthority> authorities(Set<AccountRole> roles) {
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.name()))
+                .collect(Collectors.toSet());
     }
 
 }
