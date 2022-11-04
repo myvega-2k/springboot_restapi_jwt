49498f409c01ce413d3196b2366bc0eedf66b950
myvega-2k <vega2k_@daum.net>
2022-11-04 16:40:35 +0900

    security config update

--- src/main/java/com/basic/myrestapi/common/filter/AuthenticationFilter.java
+++ src/main/java/com/basic/myrestapi/common/filter/AuthenticationFilter.java
@@ -1,8 +1,23 @@
 package com.basic.myrestapi.common.filter;
 
+import com.basic.myrestapi.accounts.Account;
+import com.basic.myrestapi.accounts.AccountAdapter;
+import com.basic.myrestapi.common.RequestLogin;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     private Environment env;
@@ -12,4 +27,34 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
         super.setAuthenticationManager(authenticationManager);
     }
 
+    @Override
+    public Authentication attemptAuthentication(
+            HttpServletRequest request,
+            HttpServletResponse response) throws AuthenticationException {
+        try {
+            RequestLogin requestLogin =
+                    new ObjectMapper().readValue(request.getInputStream(), RequestLogin.class);
+            return getAuthenticationManager().authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            requestLogin.getEmail(),
+                            requestLogin.getPassword(),
+                            new ArrayList<>()
+                    )
+            );
+        } catch(IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain chain,
+            Authentication authResult) throws IOException, ServletException {
+        //String userName = ((User)authResult.getPrincipal()).getUsername();
+        AccountAdapter accountAdapter = (AccountAdapter) authResult.getPrincipal();
+        Account account = accountAdapter.getAccount();
+        System.out.println("===> 인증성공!! : " + account.getEmail());
+    }
 }
